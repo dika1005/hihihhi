c8ccd87b24f37d6fcd933e954d0999f679116973
dika ramadani <dikabangkok21@gmail.com>
2025-06-09 22:12:55 +0700

    Add Navbar component and update layout structure; create About and Profile pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,8 @@
-
-
-export default function Home() {
+export default function about() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hello World
+      <h1 className="text-2xl">About Page</h1>
+      <p>This is the about page of our application.</p>
     </div>
   );
 }
--- src/app/about/profile/page.tsx
+++ src/app/about/profile/page.tsx
@@ -1,9 +1,9 @@
 
-
-export default function Home() {
+export default function Profile() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hello World
+      <h1 className="text-2xl">Profile Page</h1>
+      <p>This is the profile page of our application.</p>
     </div>
   );
 }
